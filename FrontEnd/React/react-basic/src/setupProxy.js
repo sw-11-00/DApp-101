f8fcdc51b350db8a83bd3b63b8a267b93b72137b
michaelach302 <118336719+michaelach302@users.noreply.github.com>
2022-11-17 20:05:22 +0800

    proxy

--- FrontEnd/React/react-basic/src/setupProxy.js
+++ FrontEnd/React/react-basic/src/setupProxy.js
@@ -2,10 +2,10 @@ const prox = require('http-proxy-middleware')
 
 module.exports = function (app) {
     app.use(
-        prox('/api1', {
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-            pathRewrite: { '^/api1': '' }
+        prox('/api1', { // 遇见api1前缀的请求，就会触发该代理配置
+            target: 'http://localhost:5000', // 请求转发给谁
+            changeOrigin: true, // 控制服务器收到的请求头中Host字段的值
+            pathRewrite: { '^/api1': '' } // 重写请求路径(必须)
         }),
         prox('/api2', {
             target: 'http://localhost:5000',
